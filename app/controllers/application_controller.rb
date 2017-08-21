1fabe35fdb81b66a171eceb2b99617e78085ba16
lgants <lgants@gmail.com>
2017-08-21 13:06:43 -0500

    completed app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # Define the Entry object
+  class Entry
+    def initialize(title, link)
+      @title = title
+      @link = link
+    end
+    attr_reader :title
+    attr_reader :link
+  end
+
+  def scrape_reddit
+    # Pull in the page
+    require 'open-uri'
+    doc = Nokogiri::HTML(open("https://www.reddit.com/"))
+
+    entries = doc.css('.entry')
+    @entriesArray = []
+    entries.each do |entry|
+      title = entry.css('p.title>a').text
+      link = entry.css('p.title>a')[0]['href']
+      @entriesArray << Entry.new(title, link)
+    end
+
+    render template: 'scrape_reddit'
+  end
 end
